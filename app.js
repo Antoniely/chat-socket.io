a9811f353169b1fa39a50622dedb612be5237518
Antoniely Robles <antonielyrobles@gmail.com>
2018-09-04 19:04:50 -0400

    Ejemplo de chat con socket.io

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
-'use strict';
 
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-//const cors = require('cors');
 const app = express();
 
 
 // settings
 app.set('port', process.env.PORT || 3000);
 
+//static public
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
-//app.use(cors({origin: 'http://localhost:4200'}));
 
 //Routes
 
-
-
 //Starting the server
-app.listen(app.get('port'), () => {
+const server = app.listen(app.get('port'), () => {
     console.log(`App listening on port ${app.get('port')}`);
 });
+
+const socket = require('socket.io');
+const io = socket(server);
+
+// WebSocket
+io.on('connection', (socket) => {
+   console.log('new connection', socket.id);
+
+  socket.on('chat:message', (data) => {
+     io.sockets.emit('chat:message', data);
+  });
+
+  socket.on('chat:typing', (data)=>{
+      socket.broadcast.emit('chat:typing', data);
+  });
+});
+
+
+
+
+
